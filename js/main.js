6358af6da8c386c7567966b5da7929602516aa5c
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-10-27 20:23:19 +0500

    Реализует авторизацию

--- js/main.js
+++ js/main.js
@@ -1,10 +1,99 @@
-const cartButton = document.querySelector("#cart-button");
-const modal = document.querySelector(".modal");
-const close = document.querySelector(".close");
+const cartButton = document.querySelector(`#cart-button`);
+const modal = document.querySelector(`.modal`);
+const close = document.querySelector(`.close`);
+const authBtn = document.querySelector(`.button-auth`);
+const modalAuth = document.querySelector(`.modal-auth`);
+const closeAuth = modalAuth.querySelector(`.close-auth`);
+const loginForm = modalAuth.querySelector(`#logInForm`);
+const loginInput = modalAuth.querySelector(`#login`);
+const passInput = modalAuth.querySelector(`#password`);
+const buttonOut = document.querySelector(`.button-out`);
+const userName = document.querySelector(`.user-name`);
+let isAuth = false;
+let login = localStorage.getItem('delivery');
+let pass = '';
 
-cartButton.addEventListener("click", toggleModal);
-close.addEventListener("click", toggleModal);
+toggleModal = () => {
+    modal.classList.toggle(`is-open`);
+};
 
-function toggleModal() {
-  modal.classList.toggle("is-open");
-}
+cartButton.addEventListener(`click`, toggleModal);
+close.addEventListener(`click`, toggleModal);
+
+// Переключить модалку авторизации
+const toggleModalAuth = (evt) => {
+    modalAuth.classList.toggle(`is-open`);
+    if (modalAuth.classList.contains(`is-open`)) {
+        loginInput.focus();
+    }
+};
+
+// Закрываем модалку авторизации
+const closeModalAuth = () => {
+    modalAuth.classList.remove(`is-open`);
+};
+
+// Логаут
+const logout = () => {
+    localStorage.removeItem(`delivery`);
+    authBtn.style.display = `block`;
+    buttonOut.removeEventListener(`click`, logout);
+    buttonOut.style.display = `none`;
+    userName.textContent = '';
+    userName.style.display = `none`;
+    isAuth = false;
+    login = null;
+    pass = '';
+    checkAuth();
+};
+
+// Пользователь авторизован
+const isAuthorized = () => {
+    console.log('Авторизован');
+
+    authBtn.style.display = `none`;
+    buttonOut.style.display = `block`;
+    userName.textContent = login;
+    userName.style.display = `inline`;
+
+    buttonOut.addEventListener(`click`, logout);
+};
+
+// Проверка авторизации
+const checkAuth = () => {
+    if (isAuth) {
+        isAuthorized();
+    } else {
+        isNotAuthorized();
+    }
+};
+
+// Логин
+const authorization = (evt) => {
+    evt.preventDefault();
+    login = loginInput.value;
+
+    localStorage.setItem('delivery', login);
+
+    pass = passInput.value;
+    isAuth = true;
+    toggleModalAuth();
+    loginForm.reset();
+
+    authBtn.removeEventListener(`click`, toggleModalAuth);
+    closeAuth.removeEventListener(`click`, closeModalAuth);
+    loginForm.removeEventListener(`submit`, authorization);
+
+    checkAuth();
+};
+
+// Пользователь НЕ авторизован
+const isNotAuthorized = () => {
+    console.log('Не авторизован');
+
+    authBtn.addEventListener(`click`, toggleModalAuth);
+    closeAuth.addEventListener(`click`, closeModalAuth);
+    loginForm.addEventListener(`submit`, authorization);
+};
+
+checkAuth();
