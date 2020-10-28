0d7ec03a91ff3effddf1af2bfdc1dcb7f2c17223
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-10-28 19:39:11 +0500

    Добавляет рендеринг карточек пиццы

--- js/rendering_cards.js
+++ js/rendering_cards.js
@@ -3,6 +3,7 @@ const containerPromo = document.querySelector(`.container-promo`);
 const restaurants = document.querySelector(`.restaurants`);
 const menu = document.querySelector(`.menu`);
 const logo = document.querySelector(`.logo`);
+const cardsMenu = document.querySelector(`.cards-menu`);
 
 const openGoods = (evt) => {
     const target = evt.target;
@@ -12,38 +13,16 @@ const openGoods = (evt) => {
         containerPromo.classList.add(`hide`);
         restaurants.classList.add(`hide`);
         menu.classList.remove(`hide`);
+
+        renderPizzaCard();
+        renderPizzaCard();
+        renderPizzaCard();
     }
 };
 
-// Добавляет карточку товара
-const createCardsRestaurant = () => {
-    const card = `
-      <a class="card card-restaurant">
-        <img src="img/tanuki/preview.jpg" alt="image" class="card-image" />
-        <div class="card-text">
-            <div class="card-heading">
-                <h3 class="card-title">Тануки</h3>
-                <span class="card-tag tag">60 мин</span>
-            </div>
-            <div class="card-info">
-                <div class="rating">
-                    4.5
-                </div>
-                <div class="price">От 1 200 ₽</div>
-                <div class="category">Суши, роллы</div>
-            </div>
-        </div>
-      </a>
-    `;
-
-    cardsRestaurants.insertAdjacentHTML(`beforeend`, card);
-};
-
-createCardsRestaurant();
-
 // Создание карточки пиццы
-const createCard = () => {
-    const card = `<div class="card">
+const renderPizzaCard = () => {
+    const pizzaCard = `<div class="card">
       <img src="img/pizza-plus/pizza-classic.jpg" alt="image" class="card-image" />
       <div class="card-text">
           <div class="card-heading">
@@ -62,8 +41,35 @@ const createCard = () => {
           </div>
       </div>
     </div>`;
+
+    cardsMenu.insertAdjacentHTML(`beforeend`, pizzaCard);
 };
 
+// Добавляет карточку товара
+const createRestaurantCard = () => {
+    const card = `<a class="card card-restaurant">
+        <img src="img/tanuki/preview.jpg" alt="image" class="card-image" />
+        <div class="card-text">
+            <div class="card-heading">
+                <h3 class="card-title">Тануки</h3>
+                <span class="card-tag tag">60 мин</span>
+            </div>
+            <div class="card-info">
+                <div class="rating">
+                    4.5
+                </div>
+                <div class="price">От 1 200 ₽</div>
+                <div class="category">Суши, роллы</div>
+            </div>
+        </div>
+      </a>`;
+
+    cardsRestaurants.insertAdjacentHTML(`beforeend`, card);
+};
+
+createRestaurantCard();
+
+// Клик по логотипу
 const logoClickHandler = () => {
     containerPromo.classList.remove(`hide`);
     restaurants.classList.remove(`hide`);
