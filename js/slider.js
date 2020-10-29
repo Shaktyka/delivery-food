80f08a3a74aae69f5c29874abba53ed8059a9e98
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-10-29 22:31:37 +0500

    Включает слайдер + настраивает параметры

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,15 @@
 new Swiper(`.swiper-container`, {
     loop: true,
     sliderPerView: 1,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+    autoplay: {
+        delay: 3000,
+    },
+    mousewheel: {
+        invert: true,
+    },
+    grabCursor: true,
 });
