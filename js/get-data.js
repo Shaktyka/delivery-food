79b67c2154920f84324f015449775606fe8f5869
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-10-29 09:27:08 +0500

    Добавляет функцию получения данных

---
+++ js/get-data.js
@@ -0,0 +1,10 @@
+// Получаем данные (асинхронно)
+const getData = async(url) => {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+        throw new Error(`Ошибка по адресу ${url}, статус ошибки ${response.status}!`);
+    }
+
+    return await response.json();
+};
