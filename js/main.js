74eb31eb4c5ae166a4e1fa3d7ea6cd8d10bc1730
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-10-30 18:55:53 +0500

    Добавлет рендеринг списка в корзине

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@
 
 const cartButton = document.querySelector(`#cart-button`);
 const modal = document.querySelector(`.modal`);
+const closeCartBtn = modal.querySelector(`.close`);
+const clearCartBtn = modal.querySelector(`.clear-cart`);
+const sendCartBtn = modal.querySelector(`.send-cart`);
+const cartBody = modal.querySelector(`.modal-body`);
+
 const close = document.querySelector(`.close`);
 const authBtn = document.querySelector(`.button-auth`);
 const modalAuth = document.querySelector(`.modal-auth`);
@@ -19,7 +24,55 @@ const toggleModal = () => {
     modal.classList.toggle(`is-open`);
 };
 
-cartButton.addEventListener(`click`, toggleModal);
+// Закрытие модалки корзины
+const closeCart = () => {
+    modal.classList.remove(`is-open`);
+    closeCartBtn.removeEventListener(`click`, closeCart);
+    clearCartBtn.removeEventListener(`click`, clearCart);
+};
+
+// Очистка корзины
+const clearCart = () => {
+    closeCart();
+};
+
+// Рендеринг 1 строки корзины
+const renderCartRow = (data) => {
+    const { id, name, price, count } = data;
+
+    return `<div class="food-row" id="${id}">
+      <span class="food-name">${name}</span>
+      <strong class="food-price">${price} ₽</strong>
+      <div class="food-counter">
+        <button class="counter-button">-</button>
+        <span class="counter">${count}</span>
+        <button class="counter-button">+</button>
+      </div>
+    </div>`.trim();
+};
+
+// Рендеринг корзины
+const renderCart = () => {
+    cartBody.innerHTML = ``;
+    console.log(cartList);
+
+    if (cartList.length > 0) {
+        cartList.forEach((item) => {
+            const cartRow = renderCartRow(item);
+            cartBody.insertAdjacentHTML(`beforeend`, cartRow);
+        });
+    }
+};
+
+// Открытие модалки корзины
+const openCart = () => {
+    renderCart();
+    modal.classList.add(`is-open`);
+    closeCartBtn.addEventListener(`click`, closeCart);
+    clearCartBtn.addEventListener(`click`, clearCart);
+};
+
+cartButton.addEventListener(`click`, openCart);
 close.addEventListener(`click`, toggleModal);
 
 // Переключить модалку авторизации
